5d015b6f8532ae9daa24ac09ccb595a35c0e32c9
philippe lebel <philippe.lebel008@sympatico.ca>
2017-06-16 20:40:25 -0400

    modified the way joint_States are aquired from gazebo, simplified the code, other cool things also

--- jaco_joystick_and_real_world/joint_states_listener/srv_gen/lisp/joint_states_listener-srv.asd
+++
@@ -1,9 +0,0 @@
-
-(cl:in-package :asdf)
-
-(defsystem "joint_states_listener-srv"
-  :depends-on (:roslisp-msg-protocol :roslisp-utils )
-  :components ((:file "_package")
-    (:file "ReturnJointStates" :depends-on ("_package_ReturnJointStates"))
-    (:file "_package_ReturnJointStates" :depends-on ("_package"))
-  ))
---
+++ jaco_joystick_and_real_world/main.py
@@ -0,0 +1,43 @@
+import argparse
+import rospy
+import roslaunch
+import os
+import threading
+import time
+
+def argumentParser(argument):
+    """ Argument parser """
+    parser = argparse.ArgumentParser(description='Either use joystick to move robot in gazebo or links a real robot to '
+                                                 'the gazebo robot')
+    parser.add_argument('control_type', metavar='control_type', type=str, default='gazebo',
+                        help='control_type = Gazebo: Only in gazebo environement. Control_type = real_world: links real'
+                             'robot to gazebo robot')
+    args_ = parser.parse_args(argument)
+    return args_
+
+if __name__ == '__main__':
+
+    args = argumentParser(None)
+    if (args.control_type == 'gazebo') or (args.control_type == 'real_world'):
+        try:
+            uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+            roslaunch.configure_logging(uuid)
+            launch = roslaunch.parent.ROSLaunchParent(uuid, ["/home/phil/catkin_ws/src/jaco-arm-pkgs/jaco_tutorial/jaco"
+                                                             "_on_table/launch/jaco_on_table_gazebo_controlled.launch"])
+
+            # package = 'jaco_on_table'
+            # executable = 'jaco_on_table_gazebo_controlled'
+            # node = roslaunch.Node(package, executable)
+            #launch = roslaunch.scriptapi.ROSLaunch()
+            launch.start()
+            time.sleep(10)
+            client_thread = threading.Thread(target=os.system("python /home/phil/catkin_ws/src/jaco-arm-pkgs/"
+                                                              "jaco_joystick_and_real_world/scripts/"
+                                                              "gazebo_realworld_linker.py gazebo"))
+            client_thread.start()
+
+
+        except rospy.ROSInterruptException:
+            pass
+    else:
+        print('wrong inputs try "-h" to get help')
